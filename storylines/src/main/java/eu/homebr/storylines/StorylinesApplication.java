4c91ec7ad10538357fd76a01fceedcfa8a39f7b6
mvvasilev <me@mvvasilev.dev>
2024-01-21 20:58:03 +0200

    Initial commit

---
+++ storylines/src/main/java/eu/homebr/storylines/StorylinesApplication.java
@@ -0,0 +1,13 @@
+package eu.homebr.storylines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorylinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorylinesApplication.class, args);
+    }
+
+}
