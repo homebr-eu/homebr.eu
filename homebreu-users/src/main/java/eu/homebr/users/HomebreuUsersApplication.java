e2cd67b130e4dfc7d6f371f8623ca5230c80e7d5
mvvasilev <me@mvvasilev.dev>
2024-01-22 17:59:52 +0200

    Add Homebreu Users API

---
+++ homebreu-users/src/main/java/eu/homebr/users/HomebreuUsersApplication.java
@@ -0,0 +1,13 @@
+package eu.homebr.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebreuUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebreuUsersApplication.class, args);
+	}
+
+}
